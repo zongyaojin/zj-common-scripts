c28f9023fc37d3052c93b751946d459bdded63f8
Zongyao Jin <zongyaojin@outlook.com>
2024-03-27 11:09:59 +0800

    cmake argument -d tweaking

--- zj-thirdparty-definitions.bash
+++ zj-thirdparty-definitions.bash
@@ -17,11 +17,11 @@ package_thirdparty_install_path=$package_thirdparty_path/install
 export zj_thirdparth_build_prefix="build"
 
 # Prioritize `$package_thirdparty_install_path` over apt install versions by setting `CMAKE_PREFIX_PATH`.
-export zj_thirdparty_cmake_args="-D CMAKE_BUILD_TYPE=Release \
-                                 -D BUILD_SHARED_LIBS=OFF \
-                                 -D CMAKE_POSITION_INDEPENDENT_CODE=ON \
-                                 -D CMAKE_PREFIX_PATH=$package_thirdparty_install_path \
-                                 -D CMAKE_INSTALL_PREFIX=$package_thirdparty_install_path"
+export zj_thirdparty_cmake_args="-DCMAKE_BUILD_TYPE=Release \
+                                 -DBUILD_SHARED_LIBS=OFF \
+                                 -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+                                 -DCMAKE_PREFIX_PATH=$package_thirdparty_install_path \
+                                 -DCMAKE_INSTALL_PREFIX=$package_thirdparty_install_path"
 
 # Add installed executables in `bin/`, if any, to system `PATH`.
 PATH="$PATH:$package_thirdparty_install_path/bin"
